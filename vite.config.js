2be04f9db6c8d0f427338a07ab431df163b90f01
smaffit285-sys <smaffit285@gmail.com>
2026-04-27 17:48:57 -0400

    Add explicit rollupOptions input pointing to index.html

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src'
+    plugins: [react()],
+    resolve: {
+          alias: {
+                  '@': '/src'
+          }
+    },
+    build: {
+          rollupOptions: {
+                  input: './index.html'
+          }
     }
-  }
 })
