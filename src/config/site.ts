64bb5925405180fae054fd5721f6ddac95e4fbfc
smaffit285-sys <smaffit285@gmail.com>
2026-08-04 00:26:24 -0400

    Point Google review CTA to review dialog

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,3 +1,3 @@
-export const GOOGLE_BUSINESS_URL = "https://maps.app.goo.gl/jgfe2GZsa7nA2VsL9";
+export const GOOGLE_BUSINESS_URL = "https://www.google.com/search?q=Miami+Knife+Guy&ludocid=11418040362576623957#lrd=0xa7ab485ee4b470eb:0x9e750736e273b955,3,,,,";
 export const GOOGLE_REVIEW_COUNT = 13;
 
